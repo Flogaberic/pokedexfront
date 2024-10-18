3fa8a67228fa885ab70205b0075464970417b295
GFONOLLOSA <98556518+Flogaberic@users.noreply.github.com>
2024-10-18 14:21:51 +0200

    style colors Pokemons

--- src/app/pages/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pages/pokemon-detail/pokemon-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {Pokemon} from "../../shared/interfaces/pokemon";
-import {ApiService} from "../../shared/services/api.service";
-import {ActivatedRoute} from "@angular/router";
+import { Pokemon } from "../../shared/interfaces/pokemon";
+import { ApiService } from "../../shared/services/api.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -11,6 +11,76 @@ import {ActivatedRoute} from "@angular/router";
 export class PokemonDetailComponent {
 
   pokemon!: Pokemon;
+
+  private readonly TYPE_NAMES: { [key: number]: string } = {
+    1: 'normal',
+    2: 'fighting',
+    3: 'flying',
+    4: 'poison',
+    5: 'ground',
+    6: 'rock',
+    7: 'bug',
+    8: 'ghost',
+    9: 'steel',
+    10: 'fire',
+    11: 'water',
+    12: 'grass',
+    13: 'electric',
+    14: 'psychic',
+    15: 'ice',
+    16: 'dragon',
+    17: 'dark',
+    18: 'fairy',
+    19: 'stellar'
+  };
+
+  private readonly TYPE_COLORS: { [key: string]: string } = {
+    normal: '#A8A77A',
+    fighting: '#C22E28',
+    flying: '#A98FF3',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    rock: '#B6A136',
+    bug: '#A6B91A',
+    ghost: '#735797',
+    steel: '#B7B7CE',
+    fire: '#EE8130',
+    water: '#6390F0',
+    grass: '#7AC74C',
+    electric: '#F7D02C',
+    psychic: '#F95587',
+    ice: '#96D9D6',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    fairy: '#D685AD',
+    stellar: '#E2E2E2' 
+  };
+
+  getTypeNames(): string[] {
+    if (!this.pokemon || !this.pokemon.default_variety.types) {
+      return [];
+    }
+
+    // Map les numéros de types aux noms des types
+    return this.pokemon.default_variety.types.map((type: any) => this.TYPE_NAMES[type.id]);
+  }
+
+  getBackgroundStyle(): string {
+    const types = this.getTypeNames();
+
+    if (types.length === 1) {
+      // Un seul type, couleur unie
+      return this.TYPE_COLORS[types[0]];
+    } else if (types.length === 2) {
+      // Deux types, appliquer un dégradé
+      const color1 = this.TYPE_COLORS[types[0]];
+      const color2 = this.TYPE_COLORS[types[1]];
+      return `linear-gradient(to right, ${color1}, ${color2})`;
+    }
+    return '';
+  } 
+
+
 
   constructor(
     private apiService: ApiService,
@@ -26,5 +96,18 @@ export class PokemonDetailComponent {
             });
       }
     });
+  }
+
+  playSound() {
+    if (this.pokemon && this.pokemon.default_variety.cry_url) {
+      const audio = new Audio();
+      audio.src = this.pokemon.default_variety.cry_url; // Utilisation de l'URL du son du Pokémon
+      audio.load();
+      audio.play().catch(error => {
+        console.error('Erreur lors de la lecture du son :', error);
+      });
+    } else {
+      console.error('Aucune URL de son trouvée pour ce Pokémon');
+    }
   }
 }
--- src/app/pages/pokemon-list/pokemon-list.component.ts
+++ src/app/pages/pokemon-list/pokemon-list.component.ts
@@ -14,6 +14,77 @@ export class PokemonListComponent {
   isLoading: boolean = false; // Ajoute un indicateur pour empêcher le double chargement
   hasMorePages: boolean = true; // Indique s'il y a encore des pages à charger
 
+  pokemon!: Pokemon;
+
+  private readonly TYPE_NAMES: { [key: number]: string } = {
+    1: 'normal',
+    2: 'fighting',
+    3: 'flying',
+    4: 'poison',
+    5: 'ground',
+    6: 'rock',
+    7: 'bug',
+    8: 'ghost',
+    9: 'steel',
+    10: 'fire',
+    11: 'water',
+    12: 'grass',
+    13: 'electric',
+    14: 'psychic',
+    15: 'ice',
+    16: 'dragon',
+    17: 'dark',
+    18: 'fairy',
+    19: 'stellar'
+  };
+
+  private readonly TYPE_COLORS: { [key: string]: string } = {
+    normal: '#A8A77A',
+    fighting: '#C22E28',
+    flying: '#A98FF3',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    rock: '#B6A136',
+    bug: '#A6B91A',
+    ghost: '#735797',
+    steel: '#B7B7CE',
+    fire: '#EE8130',
+    water: '#6390F0',
+    grass: '#7AC74C',
+    electric: '#F7D02C',
+    psychic: '#F95587',
+    ice: '#96D9D6',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    fairy: '#D685AD',
+    stellar: '#E2E2E2' 
+  };
+
+  getTypeNames(): string[] {
+    if (!this.pokemon || !this.pokemon.default_variety.types) {
+      return [];
+    }
+
+    // Map les numéros de types aux noms des types
+    return this.pokemon.default_variety.types.map((type: any) => this.TYPE_NAMES[type.id]);
+  }
+
+  getBackgroundStyle(): string {
+    const types = this.getTypeNames();
+
+    if (types.length === 1) {
+      // Un seul type, couleur unie
+      return this.TYPE_COLORS[types[0]];
+    } else if (types.length === 2) {
+      // Deux types, appliquer un dégradé
+      const color1 = this.TYPE_COLORS[types[0]];
+      const color2 = this.TYPE_COLORS[types[1]];
+      return `linear-gradient(to right, ${color1}, ${color2})`;
+    }
+    return '';
+  } 
+
+
   constructor(
     public apiService: ApiService,
   ) {
